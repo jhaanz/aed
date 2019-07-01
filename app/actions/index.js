e03230faddcaab0ce54dcbe1c41aa77fa4259848
Roxanne <gail@commlinked.ph>
2019-07-01 17:31:30 +0800

    new

--- app/actions/index.js
+++ app/actions/index.js
@@ -15,8 +15,23 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-    .then(user => {
-      dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user});
+    .then(user => loginUserSuccess(dispatch, user))
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(user => loginUserSuccess(dispatch, user))
+      .catch(() => loginUserFail(dispatch));
     });
   };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch ({
+      type: 'LOGIN_USER_SUCCESS',
+      payload: user
+  });
+};
+const loginUserFail = (dispatch) => {
+  dispatch ({
+      type: 'LOGIN_USER_FAIL'
+  });
 };
--- app/reducers/AuthReducer.js
+++ app/reducers/AuthReducer.js
@@ -1,4 +1,4 @@
-const INITIAL_STATE = { email: '', password: ''};
+const INITIAL_STATE = { email: '', password: '', user: null};
 
 export default (state = INITIAL_STATE,action) => {
   switch (action.type) {
@@ -6,7 +6,8 @@ export default (state = INITIAL_STATE,action) => {
       return { ...state, email: action.payload };
     case 'password_change':
       return { ...state, password: action.payload };
-
+    case 'LOGIN_USER_SUCCESS':
+      return { ...state, user: action.payload };
     default:
       return state;
   }
